50c9e5a6ec64d21ecda51288adf2f9e87ff50c9d
c00z <zcusimano@gmail.com>
2017-01-05 15:30:58 -0800

    finished part 1 sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,8 +10,9 @@ angular
   // ^ the first argument is a string naming the controller,
   // the second argument is a function that defines the capacities
   // of the controller.
+AlbumsIndexController.$inject = ['$http'];
 
-function AlbumsIndexController () {
+function AlbumsIndexController ( $http ) {
   var vm = this;
   vm.newAlbum = {};
 
@@ -21,17 +22,18 @@ function AlbumsIndexController () {
   };
 
   vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
   ];
+
+  $http({
+   method: 'GET',
+   url: '/api/albums'
+ }).then(function successCallback(response) {
+   console.log(response);
+    vm.albums = response.data;
+ }, function errorCallback(response) {
+   console.log('There was an error getting the data', response);
+ });
+
+
+
 }
