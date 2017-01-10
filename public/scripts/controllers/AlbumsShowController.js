97f2f338d9504c73e87fc74c255db48e63020df8
c00z <zcusimano@gmail.com>
2017-01-09 16:16:30 -0800

    finished sprint 5

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,4 +17,60 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
+vm.createSong = function () {
+  $http({
+    method: 'POST',
+    url: '/api/albums/'+$routeParams.id +'/songs',
+    data: vm.newSong
+  }).then(function songCallback(response) {
+    vm.album.songs.push(response.data);
+  }, function errorCallback(response) {
+    console.log('There was an error posting the data', response);
+  });
+  }
+
+vm.deleteSong = function (song){
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/'+$routeParams.id +'/songs/' + song._id
+    }).then(function deleteSuccess(response){
+      console.log(response, "this is the deleted song");
+      var index = vm.album.songs.indexOf(song);
+      vm.album.songs.splice(index, 1);
+    }), function deleteError (response){
+      console.log("ERROR!", response);
+    }
+  }
+
+
+  vm.editingSong = function (song) {
+    $http({
+    method: 'PUT',
+    url:'/api/albums/'+$routeParams.id +'/songs/' + song._id,
+    data: song
+  }).then(function successCallback(editSong) {
+    console.log("EDIT IS WERKIN",editSong);
+    vm.song = editSong.data;
+    // $location.path('/');
+  }, function errorCallback(response) {
+    console.log('There was an error updating the data', response);
+  });
+  }
+
+
+  // $http({
+  //   method: 'GET',
+  //   url: 'https://api.spotify.com/v1/search' + album.Name
+  // }).then(function successCallback(json) {
+  //
+  //   console.log('https://api.spotify.com/v1/search' + album.Name)
+  //   // vm.album = json.data;
+  // }, function errorCallback(response) {
+  //   console.log('There was an error getting the data', response);
+  // });
+  //
+
+
+
+
 }
